2710bc138709aff92474beb010bb03e0497edbef
MarioE99 <meev2399@outlook.com>
2024-01-11 11:40:04 -0600

    se agrego curriculum y modificado css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+// Boton de menu responsivo
+
 const mobileMenuButton = document.getElementById('mobile-menu');
 const navMenu = document.getElementById('menu');
 
 mobileMenuButton.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+
+
+//descargar CURRI
+
+var urlDocumento = '';
+
